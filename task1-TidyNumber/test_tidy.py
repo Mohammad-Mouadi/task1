080ddce2c00043905be6be53c56c5c74beeba455
Mohammad Mouadi <mmouadi@asaltech.com>
2020-06-18 11:25:08 +0300

    removed task1 empty folder

--- task1-TidyNumber/test_tidy.py
+++ task1-TidyNumber/test_tidy.py
@@ -1,25 +1,25 @@
 import unittest
 
-from TidyNumberGenerator import generate_greatest_tidy
+from TidyNumberGenerator import generate_greatest_tidy_number
 
 
 class TestTidyNumberGenerator(unittest.TestCase):
 
     def test_case_general(self):
-        self.assertEqual(generate_greatest_tidy("132"), 129)
-        self.assertEqual(generate_greatest_tidy("+132"), 129)
+        self.assertEqual(generate_greatest_tidy_number("132"), 129)
+        self.assertEqual(generate_greatest_tidy_number("+132"), 129)
 
     def test_long_edge_case(self):
-        self.assertEqual(generate_greatest_tidy("1333333333333333333333333333333333333333333332"),
+        self.assertEqual(generate_greatest_tidy_number("1333333333333333333333333333333333333333333332"),
                          1299999999999999999999999999999999999999999999)
 
     def test_negative(self):
         with self.assertRaises(Exception):
-            generate_greatest_tidy("-132")
+            generate_greatest_tidy_number("-132")
 
     def test_value(self):
         with self.assertRaises(Exception):
-            generate_greatest_tidy("-987a")
+            generate_greatest_tidy_number("-987a")
 
 
 if __name__ == '__main__':
