49a0585f3d74cf0c40aa6194584ae5022c05d56c
Mohammad Mouadi <mmouadi@asaltech.com>
2020-07-01 15:20:32 +0300

    Adding due to wrong usage of git

--- task1-TidyNumber/test_tidy.py
+++ task1-TidyNumber/test_tidy.py
@@ -8,6 +8,7 @@ class TestTidyNumberGenerator(unittest.TestCase):
     def test_case_general(self):
         self.assertEqual(generate_greatest_tidy_number("132"), 129)
         self.assertEqual(generate_greatest_tidy_number("+132"), 129)
+        self.assertEqual(generate_greatest_tidy_number(" 132  "), 129)
 
     def test_long_edge_case(self):
         self.assertEqual(generate_greatest_tidy_number("1333333333333333333333333333333333333333333332"),
@@ -20,6 +21,10 @@ class TestTidyNumberGenerator(unittest.TestCase):
     def test_value(self):
         with self.assertRaises(Exception):
             generate_greatest_tidy_number("-987a")
+
+    def test_spaces_between_digits(self):
+        with self.assertRaises(Exception):
+            generate_greatest_tidy_number("1987 47")
 
 
 if __name__ == '__main__':
