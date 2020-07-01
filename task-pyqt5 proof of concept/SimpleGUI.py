5ac89599515f580f1592b063f67dfcb49d0c431f
Mohammad Mouadi <mmouadi@asaltech.com>
2020-07-01 09:45:01 +0300

    Created exe application using Pyinstaller

--- task-pyqt5 proof of concept/SimpleGUI.py
+++ task-pyqt5 proof of concept/SimpleGUI.py
@@ -1,7 +1,7 @@
 import sys
-from PyQt5 import QtCore, QtWidgets
+
+from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
 
 
 class HelloWindow(QMainWindow):
@@ -11,20 +11,20 @@ class HelloWindow(QMainWindow):
         QMainWindow.__init__(self)
         self.setWindowTitle("Hello world program")
 
-        self.l1 = QLabel(self)
-        self.l1.move(50,50)
+        self.label = QLabel(self)
+        self.label.move(50, 50)
         self.button1 = QPushButton(self)
         self.button1.setText("Click me")
         self.button1.clicked.connect(self.button1_clicked)
 
     def button1_clicked(self):
         HelloWindow.clicked += 1
-        self.l1.setText("You have clicked " + str(HelloWindow.clicked) + " times")
-        self.l1.adjustSize()
+        self.label.setText("You have clicked " + str(HelloWindow.clicked) + " times")
+        self.label.adjustSize()
 
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     mainWin = HelloWindow()
     mainWin.show()
-    sys.exit(app.exec_())
+    app.exec_()
